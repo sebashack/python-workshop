a65c3fd373424020cf59c30e4a5b4f39104d19fd
sebashack <sebastian0092@gmail.com>
2021-10-21 09:40:48 -0500

    Set up new DB name

--- university/university/settings.py
+++ university/university/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'university.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'university.db',
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
